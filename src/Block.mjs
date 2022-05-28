0646ff045cc10dbc53d3e216c3ed5ece88ae5560
greczimarton <57832392+greczimarton@users.noreply.github.com>
2022-05-28 21:38:26 +0200

    implemented for 4 test

--- src/Block.mjs
+++ src/Block.mjs
@@ -1,7 +1,13 @@
 export class Block {
+  x;
+  y;
   color;
+  isFalling;
 
   constructor(color) {
     this.color = color;
+    this.x = 0;
+    this.y = 0;
+    this.isFalling = false;
   }
 }
--- src/Board.mjs
+++ src/Board.mjs
@@ -1,20 +1,49 @@
 export class Board {
   width;
   height;
+  blocks;
+  get oneIsFalling() {
+    for (let i = 0; i < this.blocks.length; i++) {
+      if (this.blocks[i].isFalling) return true;
+    }
+    return false;
+  }
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.blocks = new Array();
   }
 
   toString() {
     let board = "";
-    for (let i = 0; i < this.width; i++) {
-      for (let j = 0; j < this.height; j++) {
-        board += ".";
+    for (let y = this.height; 0 < y; y--) {
+      for (let x = 0; x < this.width; x++) {
+        let currentCell = "";
+        this.blocks.forEach((t) => {
+          if (t.y == y && t.x == x) currentCell = t.color;
+        });
+        board += currentCell ? currentCell : ".";
       }
       board += "\n";
     }
     return board;
+  }
+
+  drop(block) {
+    if (this.oneIsFalling) {
+      throw "already falling";
+    }
+
+    block.x = Math.floor(this.width / 2);
+    block.y = this.height;
+    block.isFalling = true;
+    this.blocks.push(block);
+  }
+
+  tick() {
+    for (let i = 0; i < this.blocks.length; i++) {
+      if (this.blocks[i].isFalling) this.blocks[i].y -= 1;
+    }
   }
 }
