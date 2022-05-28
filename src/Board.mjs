0e1a09fde32bbf8eaa7aa3e06e2cc40d2a7442fb
greczimarton <57832392+greczimarton@users.noreply.github.com>
2022-05-28 21:54:31 +0200

    implemented for 'it stops when it hits the bottom'

--- src/Board.mjs
+++ src/Board.mjs
@@ -18,7 +18,7 @@ export class Board {
 
   toString() {
     let board = "";
-    for (let y = this.height; 0 < y; y--) {
+    for (let y = this.height - 1; 0 <= y; y--) {
       for (let x = 0; x < this.width; x++) {
         let currentCell = "";
         this.blocks.forEach((t) => {
@@ -37,15 +37,17 @@ export class Board {
     }
 
     block.x = Math.floor(this.width / 2);
-    block.y = this.height;
+    block.y = this.height - 1;
     block.isFalling = true;
     this.blocks.push(block);
   }
 
   tick() {
     for (let i = 0; i < this.blocks.length; i++) {
-      if (this.blocks[i].isFalling && this.blocks[i].y - 1 >= 0)
+      if (this.blocks[i].y == 0) this.blocks[i].isFalling = false;
+      if (this.blocks[i].isFalling && this.blocks[i].y - 1 >= 0) {
         this.blocks[i].y -= 1;
+      }
     }
   }
 }
