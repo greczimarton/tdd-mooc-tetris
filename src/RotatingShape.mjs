6d58d6153332b809c02271c76f586b47a588b153
greczimarton <57832392+greczimarton@users.noreply.github.com>
2022-06-01 09:35:10 +0200

    refactored: Rotating 3x3 shape: can be rotated right/clockwise

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -21,54 +21,76 @@ export class RotatingShape {
     Object.freeze(this);
   }
 
+  /*
+    0.0 => 0.2
+    0.1 => 1.2
+    0.2 => 2.2
+    1.0 => 0.1
+    1.1 => 1.1
+    1.2 => 2.1
+    2.0 => 0.0
+    2.1 => 0.1
+    2.2 => 0.2
+  */
   rotateRight() {
     const shapeCopy = _.cloneDeep(this.shape);
-    let left = 0;
-    let right = shapeCopy.length - 1;
+    const dim = shapeCopy.length
 
-    while (left < right) {
-      for (let i = left; i <= right - 1; i++) {
-        const top = left;
-        const bottom = right;
-
-        const temp = shapeCopy[top][left + i];
-
-        shapeCopy[top][left + i] = shapeCopy[bottom - i][left];
-        shapeCopy[bottom - i][left] = shapeCopy[bottom][right - i];
-        shapeCopy[bottom][right - i] = shapeCopy[top + i][right];
-        shapeCopy[top + i][right] = temp;
+    for (let row = 0; row < dim; row++) {
+      for (let column = 0; column < dim; column++) {
+        shapeCopy[column][dim - 1 - row] = this.shape[row][column]
       }
-
-      left += 1;
-      right -= 1;
     }
 
     return new RotatingShape(shapeCopy);
+
+    // let left = 0;
+    // let right = shapeCopy.length - 1;
+
+    // while (left < right) {
+    //   for (let i = left; i <= right - 1; i++) {
+    //     const top = left;
+    //     const bottom = right;
+
+    //     const temp = shapeCopy[top][left + i];
+
+    //     shapeCopy[top][left + i] = shapeCopy[bottom - i][left];
+    //     shapeCopy[bottom - i][left] = shapeCopy[bottom][right - i];
+    //     shapeCopy[bottom][right - i] = shapeCopy[top + i][right];
+    //     shapeCopy[top + i][right] = temp;
+    //   }
+
+    //   left += 1;
+    //   right -= 1;
+    // }
+
+    // return new RotatingShape(shapeCopy);
   }
 
   rotateLeft() {
-    const shapeCopy = _.cloneDeep(this.shape);
-    let left = 0;
-    let right = shapeCopy.length - 1;
+    return this.rotateRight().rotateRight().rotateRight()
+    // const shapeCopy = _.cloneDeep(this.shape);
+    // let left = 0;
+    // let right = shapeCopy.length - 1;
 
-    while (left < right) {
-      for (let i = left; i <= right - 1; i++) {
-        const top = left;
-        const bottom = right;
+    // while (left < right) {
+    //   for (let i = left; i <= right - 1; i++) {
+    //     const top = left;
+    //     const bottom = right;
 
-        const temp = shapeCopy[top][left + i];
+    //     const temp = shapeCopy[top][left + i];
 
-        shapeCopy[top][left + i] = shapeCopy[top + i][right];
-        shapeCopy[top + i][right] = shapeCopy[bottom][right - i];
-        shapeCopy[bottom][right - i] = shapeCopy[bottom - i][left];
-        shapeCopy[bottom - i][left] = temp;
-      }
+    //     shapeCopy[top][left + i] = shapeCopy[top + i][right];
+    //     shapeCopy[top + i][right] = shapeCopy[bottom][right - i];
+    //     shapeCopy[bottom][right - i] = shapeCopy[bottom - i][left];
+    //     shapeCopy[bottom - i][left] = temp;
+    //   }
 
-      left += 1;
-      right -= 1;
-    }
+    //   left += 1;
+    //   right -= 1;
+    // }
 
-    return new RotatingShape(shapeCopy);
+    // return new RotatingShape(shapeCopy);
   }
 
 
